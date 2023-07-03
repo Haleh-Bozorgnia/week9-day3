397e134a61ded100f4fa1f59df09aed1d2db36c3
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-07-03 10:04:02 -0700

    edit codes

--- Animal App/controllers/animal.js
+++ Animal App/controllers/animal.js
@@ -1,23 +1,48 @@
-const express = require ('express')
-const Animal = require ('../models/animal')
-const router = express.Router()
+const express = require ('express');
+const Animal = require ('../models/animal');
+const router = express.Router();
+
+//controllers
 
 router.get('/',async(req,res)=> {
     const allAnimals = await Animal.find({})
-    res.render(
-        'index.ejs',
-        {animals:allAnimals}
-    )
+    res.render('animals/index.ejs',{animals:allAnimals})
 })
 
-router.get('/new', async(req,res)=> {
-    res.render('new.ejs')
+router.get('/new', (req,res)=> {
+    res.render('animals/new.ejs')
 })
 
 router.post('/', async (req,res)=>{
-    console.log(req.body)
-    req.body.extinct = req.body.extinct === on ? true : false
+    req.body.extinct = req.body.extinct === 'on' ? true : false
     await Animal.create(req.body)
     res.redirect('/animal')
 })
+
+router.get ('/:id' , async (req,res)=>{
+    const id = req.params.id
+    const animal = await Animal.findById(id)
+    res.render('animals/show.ejs',{animal})
+})
+
+router.delete('/:id',async (req,res)=>{
+    const id = req.params.id
+    await Animal.findByIdAndDelete(id);
+    res.redirect('/animal')
+})
+
+router.get ('/:id/edit', async(req,res)=>{
+    const id=req.params.id
+    const animal = await Animal.findById(id)
+    res.render('animals/edit.ejs',{animal})
+})
+
+router.put('/:id', async (req,res)=>{
+    const id=req.params.id
+    req.body.extinct = req.body.extinct === 'on' ? true:false
+    await Animal.findByIdAndUpdate(id,req.body);
+    res.redirect('/animal')
+})
+
+
 module.exports = router;
--- Animal App/server.js
+++ Animal App/server.js
@@ -2,18 +2,19 @@
 require("dotenv").config();
 //import express
 const express = require("express");
+const app = express();
+
 //import morgan
 const morgan = require("morgan");
 const AnimalRouter = require("./controllers/animal");
 //import method-override
 const methodOverride = require("method-override");
-const app = express();
 ////////////
 //middle ware
 //////////////
 app.use(express.static("public"))
 //allow to reade from the form
-app.use(express.urlencoded());
+app.use(express.urlencoded()); //allows the the req.body to be read from the form
 app.use(methodOverride("_method"));
 app.use("/animal", AnimalRouter);
 const PORT = process.env.PORT;
